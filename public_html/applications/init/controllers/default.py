137b2c2aae5d9965873665a9717474059ba28091
Raymond Elward <rayelward@gmail.com>
2011-11-04 19:12:51 -0500

    fixed moderation bug.

--- public_html/applications/init/controllers/default.py
+++ public_html/applications/init/controllers/default.py
@@ -107,3 +107,20 @@ def bg_find():
       LOAD('default','data.load',args='tables',ajax=True,user_signature=True)
     """
 ##    return dict(form=crud())
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
