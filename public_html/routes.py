21e8d755726508140cab7b57de43e52103c889fa
Raymond Elward <rayelward@gmail.com>
2011-11-05 11:01:09 -0500

    added routes.py so the long urls with app name aren't needed.

---
+++ public_html/routes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+default_application='init'
+default_controller = 'default'
+default_function = 'recents'
+
+
+routes_in = (
+             ('/recents/$p', '/init/default/recents/$p'),
+             ('/show/$p', '/init/default/show/$p'),
+             ('/$f', '/init/default/$f'),
+             ('/$c/$f', '/init/$c/$f'),
+             )
+
+
+
+routes_out = [(x, y) for (y, x) in routes_in]
+
