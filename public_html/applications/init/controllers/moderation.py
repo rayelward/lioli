5cce41af70583c311b67959170ab333bb5c107de
Raymond Elward <rayelward@gmail.com>
2011-11-15 15:14:29 -0600

    Fixed comments formatting in moderation.py.

--- public_html/applications/init/controllers/moderation.py
+++ public_html/applications/init/controllers/moderation.py
@@ -1,25 +1,23 @@
 # coding: utf8
-# try something like
 
 
-
-##this is where we give a session varible to people that know the password,
-##it allows them to access a number of other functions in the folder.
-
-##it is strongly recommeded to add a web2py reCAPTCHA to this to prevent spamming 
-#to get into the moderation side.
-#this only sets up moderation for entering new entries into the database. You must
-#use the admin interface if you want to remove something that was already entered into 
-#system.
 @auth.requires_membership(role='moderation')
 def index():
+    """
+    it is strongly recommeded to add a web2py reCAPTCHA to this to prevent spamming 
+    to get into the moderation side.
+    this only sets up moderation for entering new entries into the database. You must
+    use the admin interface if you want to remove something that was already entered into 
+    system.
+    """
     redirect(URL('moderation','new_entries'))
     return dict()
     
-#show new entries to be allowed in or deleted from the database.
 @auth.requires_membership(role='moderation')
 def new_entries():
-    
+    """
+    show new entries to be allowed in or deleted from the database.
+    """
     where_clause = (db.lioli_main.accepted == 0)
     fields = [db.lioli_main.unique_id, db.lioli_main.body, db.lioli_main.age, db.lioli_main.gender]
     rows = db(where_clause).select(limitby=(0, 10), orderby=db.lioli_main.id, *fields)
@@ -27,26 +25,31 @@ def new_entries():
     row_count = db(where_clause).count()
     return dict(rows=rows, row_count=row_count)
     
-#takes a specific entry asks for a confirmation to delete or accept, 
-#then updates database, then redirects to new_entries.
 @auth.requires_membership(role='moderation')
 def specific_entry():
+    """
+    takes a specific entry asks for a confirmation to delete or accept, 
+    then updates database, then redirects to new_entries.
+    """
     u_id = request.args(0) or redirect(URL('moderation', 'new_entries'))
     row = db(db.lioli_main.unique_id==u_id).select().first()
     return dict(row=row)
     
-    
-#this method does the actual deletion of the entry. then redirects to the new entries
 @auth.requires_membership(role='moderation')
 def delete_entry():
+    """
+    this method does the actual deletion of the entry. then redirects to the new entries
+    """
     u_id = request.args(0) or redirect(URL('moderation', 'new_entries'))
     db(db.lioli_main.unique_id == u_id).delete()
     redirect(URL('new_entries'))
     return dict()
     
-#this method does the actual accepting of an entry. then redirects to the new entries
 @auth.requires_membership(role='moderation')
 def accept_entry():
+    """
+    this method does the actual accepting of an entry. then redirects to the new entries
+    """
     u_id = request.args(0) or redirect(URL('moderation', 'new_entries'))
     db(db.lioli_main.unique_id == u_id).update(accepted=1)
     redirect(URL('new_entries'))
