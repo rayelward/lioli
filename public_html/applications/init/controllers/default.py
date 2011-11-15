33900c42c0e23de9de71d876c67e90dd7fa4c5d4
Raymond Elward <rayelward@gmail.com>
2011-11-15 15:11:34 -0600

    Fixed comment formatting in default.py.

--- public_html/applications/init/controllers/default.py
+++ public_html/applications/init/controllers/default.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
-# this file is released under public domain and you can use without limitations
-
-#########################################################################
-## This is a samples controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
-## - call exposes all registered services (none by default)
-#########################################################################
-
 
 
 def index():
+    """
+    Forwards an index request to the recents page.
+    """
     session.forget()
     redirect(URL('recents'))
     return dict()
     
 fields = [db.lioli_main.id, db.lioli_main.unique_id, db.lioli_main.body, db.lioli_main.loves, db.lioli_main.leaves, db.lioli_main.age, db.lioli_main.gender]
-#Shows 10 submissions for a user to vote on.
+
 def recents():
+    """
+    Shows 10 submissions for a user to vote on.
+    """
     page = request.vars.page or 0
     items_per_page = 5
     total_pages = db(db.lioli_main.accepted == 1).count() / 5
@@ -27,27 +23,35 @@ def recents():
     where_clause = (db.lioli_main.accepted == 1)
     rows = db(where_clause).select(limitby=(page_min, page_max), orderby=~db.lioli_main.id, *fields)
     return dict(rows=rows, total_pages=total_pages)
-    
-#Shows a random set of 10 submissions for a user to vote on.
+
 def random():
+    """
+    Shows a random set of 10 submissions for a user to vote on.
+    """
     where_clause = (db.lioli_main.accepted == 1)
     rows = db(where_clause).select(limitby=(0, 10), orderby='<random>', *fields)
     return dict(rows=rows)
 
-##uses ajax to bring up a search via keywords for people.
 def search():
+    """
+    uses ajax to bring up a search via keywords for people.
+    """
     return dict(form=FORM(INPUT(_id='keyword', _name='keyword',
         _onkeyup="ajax('bg_find', ['keyword'], 'target');")),
         target_div=DIV(_id='target'))
         
-##shows the searched for data in more detail
 def show():
+    """
+    shows the searched for data in more detail
+    """
     u_id = request.args(0) or redirect(URL('search'))
     row = db((db.lioli_main.accepted==1) & (db.lioli_main.unique_id==u_id)).select().first()
     return dict(row=row)
 
-##gets user submissions and enters them into the database.
 def submit():
+    """
+    gets user submissions and enters them into the database.
+    """
     message = 'Please input a submission'
     form = SQLFORM.factory(
         Field('body', 'text', requires=IS_NOT_EMPTY()),
@@ -59,13 +63,17 @@ def submit():
         response.flash = 'Thank you.'
     return dict(form=form, message=message)
     
-##leads to default/about.html which gives a description of the application:
 def about():
+    """
+    leads to default/about.html which gives a description of the application:
+    """
     session.forget()
     return dict()
     
-##function called by ajax to display search results
 def bg_find():
+    """
+    function called by ajax to display search results
+    """
     pattern = '%' + request.vars.keyword.lower() + '%'
     where_clause = ((db.lioli_main.body.lower().like(pattern)) |(db.lioli_main.unique_id.like(pattern))) & (db.lioli_main.accepted==1)
     pages = db(where_clause).select(orderby=('<random>'), limitby=(0,10))
@@ -78,15 +86,19 @@ def bg_find():
 
 ##functions for voting::
 
-##called by AJAX used for voting up by one 
 def add_loves():
+    """
+    called by AJAX used for voting up by one 
+    """
     row = db(db.lioli_main.id == request.vars.id).select().first()
     new_loves = row.loves + 1
     row.update_record(loves=new_loves)
     return str(new_loves)
     
-##called by AJAX used for voting up by one
 def add_leaves():
+    """
+    called by AJAX used for voting up by one
+    """
     row = db(db.lioli_main.id == request.vars.id).select().first()
     new_leaves = row.leaves + 1
     row.update_record(leaves=new_leaves)
