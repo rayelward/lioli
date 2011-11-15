4c3ff155e49c9a26451ec3d535fc50e2f52ed442
Raymond Elward <rayelward@gmail.com>
2011-11-15 15:21:02 -0600

    Fixed comments in db_lioli_main.py.

--- public_html/applications/init/models/db_lioli_main.py
+++ public_html/applications/init/models/db_lioli_main.py
@@ -9,19 +9,23 @@ db.define_table('lioli_main',
     Field('gender', notnull=True),
     Field('accepted', 'integer', default=0, notnull=True))
 
-##this is needed for the buildin python random number generator
-##gets a random 10 digit number for us.
-#IN:n/a
-#OUT:random number between 1 and 9999999999
 def rand_unique_id():
+    """
+    this is needed for the buildin python random number generator
+    gets a random 10 digit number for us.
+    IN:n/a
+    OUT:random number between 1 and 2000000000
+    """
     import random
     return random.randrange(1, 2000000000, 1)
 
-##checks if the randomly generated number is already in the database.
-##it uses tail recursion if the random number is already in the database
-#IN:n/a
-#OUT:new random unique_id
 def get_new_unique_id():
+    """
+    checks if the randomly generated number is already in the database.
+    it uses tail recursion if the random number is already in the database
+    IN:n/a
+    OUT:new random unique_id
+    """
     ran_num = rand_unique_id()
     row = db(db.lioli_main.unique_id==ran_num).select().first()
     if row == None:
@@ -29,11 +33,12 @@ def get_new_unique_id():
     else:
         return get_new_unique_id()
         
-        
-##gets input and puts it in the database, returning the new unique
-#IN:body, age, gender of post
-#OUT:unique_id for post
 def insert_into_my_db(body, age, gender):
+    """
+    gets input and puts it in the database, returning the new unique
+    IN:body, age, gender of post
+    OUT:unique_id for post
+    """
     unique_id = get_new_unique_id() 
     db.lioli_main.insert(unique_id=unique_id, body=body, age=age, gender=gender)
     return unique_id
